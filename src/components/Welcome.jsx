da1e3929be66cc564b85ac2cfc0cfed97196bd02
nelfimov <nelfimov@mail.ru>
2023-01-12 17:07:44 +0300

    refactoring welcome component

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -8,30 +8,18 @@ import {Login, Register} from './index';
 export default function Welcome() {
   const [toggle, setToggle] = useState(false);
 
-  /**
-   * Toggle which form to display - login or sign up.
-   * @return {JSX} Form to display.
-   */
-  function displayForm() {
-    if (toggle) {
-      return (
-        <>
-          <button onClick={() => setToggle(!toggle)}>Change to sign in</button>
-          <Login />
-        </>
-      );
-    }
-    return (
-      <>
-        <button onClick={() => setToggle(!toggle)}>Change to log in</button>
-        <Register />
-      </>
-    );
-  }
-
   return (
     <div className="Welcome">
-      {displayForm}
+      <button
+        className={ toggle ? 'login' : 'register'}
+        onClick={() => setToggle(!toggle)}
+      >
+        Change to { toggle ? 'sign in' : 'log in' }
+      </button>
+      { toggle ?
+        <Login /> :
+        <Register />
+      }
     </div>
   );
 }
