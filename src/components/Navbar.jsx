973bb1507f0326e28a2400110273f1a5ca24f8a7
nelfimov <nelfimov@mail.ru>
2023-01-13 17:13:01 +0300

    added images, styling to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import {NavLink} from 'react-router-dom';
 import propTypes from 'prop-types';
 import '../styles/Navbar.css';
+import {useEffect} from 'react';
 
 /**
  * Navbar component
@@ -8,21 +9,43 @@ import '../styles/Navbar.css';
  * @return {JSX} JSX
  */
 export default function Navbar({isLogged, logout}) {
+  /**
+   * Show menu on click.
+   * @param {shape} e Event object.
+   */
+  function showMenu(e) {
+    const nav = document.querySelector('nav');
+    nav.className = nav.className === 'hidden' ? 'active' : 'hidden';
+  }
+
+  useEffect(() => {
+    document.querySelectorAll('nav a, nav button')
+        .forEach((element) => {
+          element.addEventListener('click', showMenu);
+        });
+  });
+
   return (
     <header>
-      <span className="logo">ODIN-BOOK</span>
-      { isLogged ?
-      <nav>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to='/profile'>Profile</NavLink>
-        <button onClick={logout}>
-          Log out
+      <div className="header-container">
+        <span className="logo">ODIN-BOOK</span>
+        { isLogged ?
+        <nav className="active">
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/profile'>Profile</NavLink>
+          <button onClick={logout}>
+            <img src="images/icons/logout.svg" alt="Log out" />
+            Log out
+          </button>
+        </nav> :
+        <nav className="active">
+          <a href="https://github.com/nelfimov">Github</a>
+        </nav>
+        }
+        <button className="menu" onClick={showMenu}>
+          <img src="images/icons/menu.svg" alt="Menu" />
         </button>
-      </nav> :
-      <nav>
-        <a href="https://github.com/nelfimov" className="nav-link">Github</a>
-      </nav>
-      }
+      </div>
     </header>
   );
 }
