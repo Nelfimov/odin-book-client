d7279411416fe24e8388427633aca8ef74392b30
nelfimov <nelfimov@mail.ru>
2023-01-13 09:19:44 +0300

    added post component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,30 @@
+import {useEffect, useState} from 'react';
+import Post from './Post';
+
 /**
  * Home page.
  * @return {JSX} JSX
  */
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  /**
+   * Get posts.
+   */
+  async function getPosts() {
+    const response = await fetch('http://localhost:3000/posts');
+    const data = await JSON.parse(response);
+    const {posts} = data;
+    return posts;
+  }
+
+  useEffect(() => {
+    setPosts(getPosts());
+  });
+
   return (
     <main>
-
+      {posts.forEach((post) => <Post post={post}/>)}
     </main>
   );
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,26 @@
+import propTypes from 'prop-types';
+
+/**
+ * Post component.
+ * @param {shape} post Post object
+ * @return {JSX} JSX
+ */
+export default function Post({post}) {
+  return (
+    <div className="post-container">
+      <div className="top">
+        <span>{post.author.username}</span>
+        <span>{post.creadetAt}</span>
+      </div>
+      <h2>{post.title}</h2>
+      <p>{post.text}</p>
+      <div className="bottom">
+        <button type='button'>Like</button>
+      </div>
+    </div>
+  );
+}
+
+Post.propTypes = {
+  post: propTypes.object.isRequired,
+};
