8997515f583ad7cdb03925c1a9b09ed9703d0405
nelfimov <nelfimov@mail.ru>
2023-01-16 11:26:31 +0300

    added styling to buttons in posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,21 +9,32 @@ import '../styles/Post.css';
 export default function Post({post}) {
   return (
     <div className="post-container">
-      <div className="top">
-        <span>{post.author.username}</span>
-        <span>{new Date(post.createdAt).toDateString()}</span>
-      </div>
-      <h2>{post.title}</h2>
-      <p>{post.text}</p>
+
+      <a href={`posts/${post._id}`}>
+
+        <div className="top">
+          <span>{post.author.username}</span>
+          <span>{new Date(post.createdAt).toDateString()}</span>
+        </div>
+
+        <h2>{post.title}</h2>
+
+        <p>{post.text}</p>
+      </a>
+
       <div className="bottom">
+
         <button type='button'>
           <img src="images/icons/like.svg" alt="like" />
           {post.likes.count}
         </button>
-        <a href="http://">
+
+        <a href={`posts/${post._id}#comments`}>
           <img src="images/icons/comment.svg" alt="comments" />
         </a>
+
       </div>
+
     </div>
   );
 }
